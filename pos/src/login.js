6a8f81df1fde577433c7c03ae23d6d9ad97abfef
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-07-07 22:09:17 +0500

    Worked on SIgnup and Login Pages

--- pos/src/login.js
+++ pos/src/login.js
@@ -1,10 +1,49 @@
 import React from "react";
 import NavigationBar from "./navbar";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
+import { Container, Button } from 'react-bootstrap'
+
+const LoginSchema = Yup.object().shape({
+    Email: Yup.string().email('Invalid Email').required('Required'),
+    Password: Yup.string().required('Required'),
+})
+
+const initialValues = {
+    Email: '',
+    Password: '',
+}
+
+const onSubmit = (values) => {
+    console.log(values);
+}
 
 
 const Login = () => {
-    return(
-        <NavigationBar/>
+    return (
+        <>
+            <NavigationBar />
+            <div className="jumbotron">
+                <h3>Login</h3>
+            </div>
+            <Formik initialValues={initialValues} validationSchema={LoginSchema} onSubmit={(values) => onSubmit(values)}>
+                <Container>
+                    <Form>
+                        <div className="d-flex flex-column">
+                            <label className="mt-3 formLabel">Email</label>
+                            <Field name='Email' className="inputField"/>
+                            <div  className="errorMessage"><ErrorMessage name='Email' className="errorMessage"/></div>
+                            <label className="mt-3 formLabel">Password</label>
+                            <Field name='Password' type="password" className="inputField"/>
+                            <div  className="errorMessage"><ErrorMessage name='Password' className="errorMessage"/></div>
+                        </div>
+                        <Button type="submit" variant="primary" className="mt-5 mr-3" style={{float: 'right'}}>
+                            Submit
+                        </Button>
+                    </Form>
+                </Container>
+            </Formik>
+        </>
     )
 }
 
--- pos/src/signup.js
+++ pos/src/signup.js
@@ -2,13 +2,15 @@ import React from "react";
 import NavigationBar from "./navbar";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from "yup";
+import { Container, Button } from 'react-bootstrap'
 
 const SignupSchema = Yup.object().shape({
     FirstName: Yup.string().min(2, 'Too Short').max(20, 'Too Long').required('Required'),
     LastName: Yup.string().min(2, 'Too Short').max(20, 'Too Long').required('Required'),
     Email: Yup.string().email('Invalid Email').required('Required'),
-    Password: Yup.string().min(6, 'Too Short').matches(/[A-Z]/, 'Must Contain Atleast One Upper Case Letter').matches(/[a-z]/, 'Must Contain Atleast One Lower Case Letter').matches(/[0-9]/, 'Must Contain Atleast One Numeric Value').matches(/[!@#$&]/, 'Must Contain Atlease One Special Character'),
-    ConfirmPassword: Yup.string().oneOf([Yup.ref('Password'), null], 'Passwords Must Match')
+    BrandName: Yup.string().min(2, 'Too Short').max(20, 'Too Long').required('Required'),
+    Password: Yup.string().min(6, 'Too Short').matches(/[A-Z]/, 'Must Contain Atleast One Upper Case Letter').matches(/[a-z]/, 'Must Contain Atleast One Lower Case Letter').matches(/[0-9]/, 'Must Contain Atleast One Numeric Value').matches(/[!@#$&]/, 'Must Contain Atlease One Special Character').required('Required'),
+    ConfirmPassword: Yup.string().oneOf([Yup.ref('Password'), null], 'Passwords Must Match').required('Required')
 })
 
 const initialValues = {
@@ -32,19 +34,33 @@ const Signup = () => {
                 <h3>Registration</h3>
             </div>
             <Formik initialValues={initialValues} validationSchema={SignupSchema} onSubmit={(values) => onSubmit(values)}>
-                <Form>
-                    <Field name='FirstName' />
-                    <ErrorMessage name='FirstName' />
-                    <Field name='LastName' />
-                    <ErrorMessage name='LastName' />
-                    <Field name='Email' />
-                    <ErrorMessage name='Email' />
-                    <Field name='Password' />
-                    <ErrorMessage name='Password' />
-                    <Field name='ConfirmPassword' />
-                    <ErrorMessage name='ConfirmPassword' />
-                    <button type="submit">Submit</button>
-                </Form>
+                <Container>
+                    <Form>
+                        <div className="d-flex flex-column">
+                            <label className="mt-3 formLabel">First Name</label>
+                            <Field name='FirstName' className="inputField" />
+                            <div  className="errorMessage"><ErrorMessage name='FirstName'/></div>
+                            <label className="mt-3 formLabel">Last Name</label>
+                            <Field name='LastName' className="inputField"/>
+                            <div  className="errorMessage"><ErrorMessage name='LastName' className="errorMessage"/></div>
+                            <label className="mt-3 formLabel">Email</label>
+                            <Field name='Email' className="inputField"/>
+                            <div  className="errorMessage"><ErrorMessage name='Email' className="errorMessage"/></div>
+                            <label className="mt-3 formLabel">Brand Name</label>
+                            <Field name='BrandName' className="inputField"/>
+                            <div  className="errorMessage"><ErrorMessage name='BrandName' className="errorMessage"/></div>
+                            <label className="mt-3 formLabel">Password</label>
+                            <Field name='Password' type="password" className="inputField"/>
+                            <div  className="errorMessage"><ErrorMessage name='Password' className="errorMessage"/></div>
+                            <label className="mt-3 formLabel">Confirm Password</label>
+                            <Field name='ConfirmPassword' type="password" className="inputField"/>
+                            <div  className="errorMessage"><ErrorMessage name='ConfirmPassword' className="errorMessage"/></div>
+                        </div>
+                        <Button type="submit" variant="primary" className="mt-5 mr-3" style={{float: 'right'}}>
+                            Submit
+                        </Button>
+                    </Form>
+                </Container>
             </Formik>
         </>
     )
